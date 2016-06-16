6ab56ac1e72d924cae3aa8ffb9ed6fb70e76cdf2
steven <steven@koocell.com>
2016-06-16 10:15:41 +0800

    second add

--- dem1/mydem1/web/code_quickstart/mouseOverandOut.js
+++ dem1/mydem1/web/code_quickstart/mouseOverandOut.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
     
     $('span.QWE').mouseover(function(){
-        document.getElementById("reg").style.color = "red";
+        document.getElementById('reg').style.color = 'red';
         $.get('quickstart_10.html',function(data){
             $('p').html(data);
         });
     });
     
     $('span.QWE').mouseout(function(){
-        document.getElementById("reg").style.color = "black";
+        document.getElementById('reg').style.color = 'black';
     });
             
     $('span.IOP').mouseover(function(){
-        document.getElementById("log").style.color = "blue";
+        document.getElementById('log').style.color = 'blue';
         $.get('quickstart_11.html',function(data){
             $('p').html(data);
         });
     });
              
     $('span.QWE').mouseout(function(){
-        document.getElementById("reg").style.color = "black";
+        document.getElementById('reg').style.color = 'black';
     });
             
 });
