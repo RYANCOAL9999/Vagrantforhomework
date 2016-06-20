56719c75ff9bf407f8516822562681341d120fdb
RYANCOAL9999 <lmf242003@gmail.com>
2016-06-20 09:59:26 +0800

    Update validation.php

--- Ex1/validation.php
+++ Ex1/validation.php
@@ -5,7 +5,7 @@ $formfield = $_GET['field'];
 
 //Check name length is/isnot small than 5 alphabets;
 if ($formfield == "name") {
-    if (strlen($value) < 5) {
+    if (strlen($value) < 5 && ctype_alpha($value) {
         echo "Error : At least 5 alphabets.";
     } else {
         
@@ -23,7 +23,7 @@ if ($formfield == "useremail") {
 
 //Check password is/is not 5 digits. 
 if ($formfield == "password") {
-    if (strlen($value) != 5 ) {
+    if (strlen($value) != 5 && $value < 1 && !is_numeric($value) ) {
         echo "Error : Password should be 5 digits ";
     } else {
         
@@ -32,7 +32,7 @@ if ($formfield == "password") {
 
 //Check CustomerID is/is not 5 digits. 
 if ($formfield == "CustomerID") {
-    if (strlen($value) != 5 ) {
+    if (strlen($value) != 5 && $value < 1 && !is_numeric($value) ) {
         echo "Error : CustomerID should 5 digits";
     } else {
         
@@ -41,7 +41,7 @@ if ($formfield == "CustomerID") {
 
 //Check pwd is/is not 5 digits. 
 if ($formfield == "pwd") {
-    if (strlen($value) != 5 ) {
+    if (strlen($value) != 5 && $value < 1 && !is_numeric($value) ) {
         echo "Error : password should be 5 digits";
     } else {
         
