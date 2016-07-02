d1b090261f7b240c07fdf79e280e2415d3662019
RYANCOAL9999 <lmf242003@gmail.com>
2016-07-02 19:57:46 +0800

    Update validation.php

--- Ex2/validation.php
+++ Ex2/validation.php
@@ -1,14 +1,15 @@
 <?php
 $value = $_GET['query'];
 $formfield = $_GET['field'];
-//check validation with input customerID
-if(isset($formfield){ 
+
+if(isset($formfield){
+	//check validation with input customerID
 	if($formfield == "customerID"){
 		if ( strlen($value) < 7 || !ctype_alpha($value) ){
         echo "Error : At least 7 alphabets";
 		}
 	}
-//check validation with input password
+	//check validation with input password
 	if($formfield == "password"){
 		if( strlen($value) != 6 || !is_numeric($value) || strpos($value, "0")!== false ){
 			echo "Error: password should be 6 digits";
