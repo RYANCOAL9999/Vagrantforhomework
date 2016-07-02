0d7dc3ea36519e4a53477d5512915450c7960aa3
RYANCOAL9999 <lmf242003@gmail.com>
2016-07-02 19:58:53 +0800

    Update ticketvalidation.php

--- Ex2/ticketvalidation.php
+++ Ex2/ticketvalidation.php
@@ -1,77 +1,80 @@
 <?php
 $value = $_GET['query'];
 $formfield = $_GET['field'];
-//check Inventory for column1 with first film 
-if($formfield =="System1"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
-}
-//check Inventory for column2 with first film 
-if($formfield =="System2"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
-}
-//check Inventory for column3 with first film 
-if($formfield =="System3"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
-}
-//check Inventory for column4 with first film 
-if($formfield =="System4"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
-}
-//check Inventory for column1 with second film 
-if($formfield =="Space1"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
-}
-//check Inventory for column2 with second film 
-if($formfield =="Space2"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
-}
-//check Inventory for column3 with second film 
-if($formfield =="Space3"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
-}
-//check Inventory for column4 with second film 
-if($formfield =="Space4"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
-}
-//check Inventory for column1 with third film 
-if($formfield =="Way1"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
-}
-//check Inventory for column2 with third film 
-if($formfield =="Way2"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
-}
-//check Inventory for column3 with third film 
-if($formfield =="Way3"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
-}
-//check Inventory for column4 with third film 
-if($formfield =="Way4"){
-    if ( !is_numeric($value) ){
-        echo "Error :  Number of Tickets should be digits";
-    }
+//formfield is/isnot have value
+if(isset($formfield)){
+	//check Inventory for column1 with first film 
+	if($formfield =="System1"){
+		if ( !is_numeric($value) ){
+			echo "Error :  Number of Tickets should be digits";
+		}
+	}
+	//check Inventory for column2 with first film 
+	if($formfield =="System2"){
+		if ( !is_numeric($value) ){
+			echo "Error :  Number of Tickets should be digits";
+		}
+	}
+	//check Inventory for column3 with first film 
+	if($formfield =="System3"){
+		if ( !is_numeric($value) ){
+			echo "Error :  Number of Tickets should be digits";
+		}
+	}
+	//check Inventory for column4 with first film 
+	if($formfield =="System4"){
+		if ( !is_numeric($value) ){
+			echo "Error :  Number of Tickets should be digits";
+		}
+	}
+	//check Inventory for column1 with second film 
+	if($formfield =="Space1"){
+		if ( !is_numeric($value) ){
+			echo "Error :  Number of Tickets should be digits";
+		}
+	}
+	//check Inventory for column2 with second film 
+	if($formfield =="Space2"){
+		if ( !is_numeric($value) ){
+			echo "Error :  Number of Tickets should be digits";
+		}
+	}
+	//check Inventory for column3 with second film 
+	if($formfield =="Space3"){
+		if ( !is_numeric($value) ){
+			echo "Error :  Number of Tickets should be digits";
+		}
+	}
+	//check Inventory for column4 with second film 
+		if($formfield =="Space4"){
+			if ( !is_numeric($value) ){
+				echo "Error :  Number of Tickets should be digits";
+		}
+	}
+	//check Inventory for column1 with third film 
+	if($formfield =="Way1"){
+		if ( !is_numeric($value) ){
+			echo "Error :  Number of Tickets should be digits";
+		}
+	}
+	//check Inventory for column2 with third film 
+	if($formfield =="Way2"){
+		if ( !is_numeric($value) ){
+			echo "Error :  Number of Tickets should be digits";
+		}
+	}
+	//check Inventory for column3 with third film 
+	if($formfield =="Way3"){
+		if ( !is_numeric($value) ){
+			echo "Error :  Number of Tickets should be digits";
+		}
+	}
+	//check Inventory for column4 with third film 
+	if($formfield =="Way4"){
+		if ( !is_numeric($value) ){
+			echo "Error :  Number of Tickets should be digits";
+		}
+	}
 }
 ?>
 
