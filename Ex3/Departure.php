09eeae906abedb1a47e1d3c15d6d3f89f2ea606e
RYANCOAL9999 <lmf242003@gmail.com>
2016-07-05 17:47:28 +0800

    Update Departure.php

--- Ex3/Departure.php
+++ Ex3/Departure.php
@@ -1,10 +1,10 @@
 <?php
 //get db php data
 require ('db.php');
-//query for database
+//string query for database
 $query = "select * FROM `Departure`";
+//use query to ask databases
 $result = mysqli_query($connection, $query) or die(mysql_error());
-
 $emptyarray = array();
 //put the result the array
 if($result -> num_rows > 0){
@@ -14,7 +14,6 @@ if($result -> num_rows > 0){
 }
 //print out result with json
 echo json_encode($emptyarray);
-
 //close the db connection
 mysqli_close($connection);
 ?>
