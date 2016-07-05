877ca61e84a980edc7a3d9151cf8af5870f0aafb
RYANCOAL9999 <lmf242003@gmail.com>
2016-07-05 18:09:15 +0800

    Update index.php

--- Ex3/index.php
+++ Ex3/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>ZZZ International Ariport</title>
+        <title>ZZZ International Airport</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
