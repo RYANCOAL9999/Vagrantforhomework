65af7f8cf11d5730e6c6cb1a54d6fdf6cbebbdf4
RYANCOAL9999 <lmf242003@gmail.com>
2016-07-08 17:46:36 +0800

    Update Arrival.php

--- Ex3/Arrival.php
+++ Ex3/Arrival.php
@@ -4,12 +4,14 @@ $query = "select * FROM `Arrival`";
 $result = mysqli_query($connection, $query) or die(mysql_error());
 
 $emptyarray = array();
+//error handles checking with result and put to the num_rows
 if($result -> num_rows > 0){
     while ($row = $result->fetch_assoc()){
         //handle the value is null with timestamp
         if($row["Time"] == "0000-00-00 00:00:00"){
             $row["Time"]="";
         }
+        //put the row to the emptyarray
         $emptyarray[] = $row;
     }
 }
