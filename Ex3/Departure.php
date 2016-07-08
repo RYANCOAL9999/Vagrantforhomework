1a8eb88b8c5a781798d2e4a184d8f70e901b3062
RYANCOAL9999 <lmf242003@gmail.com>
2016-07-08 17:47:46 +0800

    Update Departure.php

--- Ex3/Departure.php
+++ Ex3/Departure.php
@@ -4,8 +4,10 @@ $query = "select * FROM `Departure`";
 $result = mysqli_query($connection, $query) or die(mysql_error());
 
 $emptyarray = array();
+//error handles checking with result and put to the num_rows
 if($result -> num_rows > 0){
     while ($row = $result->fetch_assoc()){
+        //put the row to the emptyarray
         $emptyarray[] = $row;
     }
 }
