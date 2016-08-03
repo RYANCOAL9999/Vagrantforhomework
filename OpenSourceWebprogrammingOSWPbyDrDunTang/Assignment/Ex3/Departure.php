803515af944acf79bc8bd78e3569c51909b25bfe
RYANCOAL9999 <lmf242003@gmail.com>
2016-08-03 16:47:46 +0800

    Update Departure.php

--- OpenSourceWebprogrammingOSWPbyDrDunTang/Assignment/Ex3/Departure.php
+++ OpenSourceWebprogrammingOSWPbyDrDunTang/Assignment/Ex3/Departure.php
@@ -9,6 +9,8 @@ $emptyarray = array();
 //error handles checking with result and put to the num_rows
 if($result -> num_rows > 0){
     while ($row = $result->fetch_assoc()){
+        $row["Terminal"] = checkTerminal($row["Terminal"]);
+        $row["Status"] = checkStatus($row["Status"]);
         //put the row to the emptyarray
         $emptyarray[] = $row;
     }
@@ -19,4 +21,23 @@ echo json_encode($emptyarray);
 
 //close the db connection
 mysqli_close($connection);
+
+function checkTerminal($data){
+     if( $data == "T1" || $data == "T2" || $data == "T3" || $data == "T4" ){
+        return $data;        
+    }
+    else{
+        return "Error";
+    }
+}
+
+function checkStatus($data){
+    if( $data == "Gate Closed" || $data == "Final Call" || $data == "Boarding" || $data == "BoardingSoon" || $data == "Cancelled" || substr($data, 0, 3) == "Est" ){
+        return $data;        
+    }
+    else{
+        return "Error";
+    }
+}
+
 ?>
