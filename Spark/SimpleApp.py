605ca34cd026082f2e98cc33aa90603328af1cd4
steven <steven@koocell.com>
2016-08-23 14:49:13 +0800

    add spark folder with zookeeper with apache to test parallel computing

---
+++ Spark/SimpleApp.py
@@ -0,0 +1,10 @@
+from pyspark import SparkContext
+
+logFile = "spark-2.0.0-bin-hadoop2.7/README.md"
+sc = SparkContext("local", "Simple App")
+logData = sc.textFile(logFile).cache()
+
+numAs = logData.filter(lambda s: 'a' in s).count()
+numBs = logData.filter(lambda s: 'b' in s).count()
+
+print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
